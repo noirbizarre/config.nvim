ce8660a03c308414546bc7b845e39799d477aa52
Axel H. <noirbizarre@gmail.com>
2024-02-25 15:28:08 +0100

    feat(keys): close buffer without closing the window

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -5,18 +5,5 @@ local opts = { noremap = true, silent = true }
 -- Normal mode
 ---
 
--- Normal mode
-
--- Move to previous/next
--- keymap("n", "<C-Left>", ":BufferLineCyclePrev<cr>", opts)
--- keymap("n", "<C-Right>", ":BufferLineCycleNext<cr>", opts)
-
--- Re-order to previous/next
--- keymap("n", "<C-M-Left>", ":BufferLineMovePrev<cr>", opts)
--- keymap("n", "<C-M-Right>", " :BufferLineMoveNext<cr>", opts)
-
--- Sidebar (NeoTree)
--- keymap("n", "<C-B>", ":Neotree<cr>", opts)
-
--- Terminal
--- keymap("n", "<C-T>", ":ToggleTerm<cr>", opts)
+-- Close buffer and display previous buffer in current window
+keymap("n", "<leader>q", ":b#<bar>bd#<cr>", opts)

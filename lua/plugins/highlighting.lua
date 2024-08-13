d3e203c164e574df3d25fd4d38de96200209a643
Axel H. <noirbizarre@gmail.com>
2024-08-13 03:00:13 +0200

    chore: treesitter config cleanup

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -1,6 +1,8 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        cmd = { "TSInstall", "TSUpdate", "TSInstallInfo", "TSBufEnable", "TSBufDisable","TSEnable", "TSDisable", "TSInstallInfo", },
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = {
@@ -68,51 +70,7 @@ return {
                 auto_install = true,
 
                 highlight = { enable = true },
-                -- indent = { enable = false },
-                -- context_commentstring = { enable = true, enable_autocmd = false },
-                -- incremental_selection = {
-                --     enable = true,
-                --     keymaps = {
-                --         init_selection = "<C-space>",
-                --         node_incremental = "<C-space>",
-                --         scope_incremental = "<C-s>",
-                --         node_decremental = "<C-bs>",
-                --     },
-                -- },
-                -- query_linter = {
-                --     enable = true,
-                --     use_virtual_text = true,
-                --     lint_events = { "BufWrite", "CursorHold" },
-                -- },
-                -- playground = {
-                --     enable = true,
-                --     disable = {},
-                --     updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-                --     persist_queries = true, -- Whether the query persists across vim sessions
-                --     keybindings = {
-                --         toggle_query_editor = "o",
-                --         toggle_hl_groups = "i",
-                --         toggle_injected_languages = "t",
-                --         toggle_anonymous_nodes = "a",
-                --         toggle_language_display = "I",
-                --         focus_language = "f",
-                --         unfocus_language = "F",
-                --         update = "R",
-                --         goto_node = "<cr>",
-                --         show_help = "?",
-                --     },
-                -- },
-                -- textobjects = {
-                --     select = {
-                --         enable = false,
-                --     },
-                --     move = {
-                --         enable = false,
-                --     },
-                --     lsp_interop = {
-                --         enable = false,
-                --     },
-                -- },
+                indent = { enable = true },
             })
         end,
     },

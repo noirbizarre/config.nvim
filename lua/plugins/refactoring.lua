34dce1d4e5aa7a28131197908f09a655bb870daa
Axel H. <noirbizarre@gmail.com>
2024-11-17 22:34:30 +0100

    feat(refactoring): debug keymaps

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -5,38 +5,58 @@ return {
         "ThePrimeagen/refactoring.nvim",
         keys = {
             {
-                "<leader>rr",
+                "<leader>rp",
                 function()
-                    require("refactoring").select_refactor()
+                    require("refactoring").debug.printf({ below = false })
                 end,
-                mode = "v",
-                noremap = true,
-                silent = true,
-                expr = false
+                desc = "Print statement",
             },
-            { "<leader>kr", function() require("telescope").extensions.refactoring.refactors() end, mode = { "v" }, desc = "Refactor", },
+            {
+                "<leader>rd",
+                function()
+                    require("refactoring").debug.print_var({})
+                end,
+                mode = { "n", "x" },
+                desc = "Print var statement",
+            },
+            {
+                "<leader>rc",
+                function()
+                    require("refactoring").debug.cleanup({})
+                end,
+                desc = "Clear print statements",
+            },
+            -- Telescope menu
+            {
+                "<leader>kr",
+                function()
+                    require("telescope").extensions.refactoring.refactors()
+                end,
+                mode = { "n", "x" },
+                desc = "Refactor",
+            },
         },
-        opts = {}
+        opts = {},
     },
-    --- Comments 
+    --- Comments
     --- https://github.com/numToStr/Comment.nvim
     {
-        'numToStr/Comment.nvim',
+        "numToStr/Comment.nvim",
         lazy = false,
         config = true,
     },
     -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-move.md
     {
-        'echasnovski/mini.move',
+        "echasnovski/mini.move",
         version = false,
         config = true,
     },
     -- Format
     -- https://github.com/stevearc/conform.nvim
     {
-        'stevearc/conform.nvim',
+        "stevearc/conform.nvim",
         event = "BufWritePre",
-    	cmd = { "ConformInfo" },
+        cmd = { "ConformInfo" },
         keys = {
             {
                 "<leader>rf",
@@ -48,24 +68,48 @@ return {
         },
         opts = {
             formatters_by_ft = {
-				lua = { "stylua" },
-                python = {'ruff_organize_imports', 'ruff_format'},
+                css = { "prettier" },
+                graphql = { "prettier" },
+                html = { "prettier" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                json = { "prettier" },
+                lua = { "stylua" },
+                markdown = { "prettier" },
+                python = { "ruff_organize_imports", "ruff_format" },
+                rust = { "rustfmt" },
                 sh = { "shfmt" },
+                svelte = { "prettier" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                yaml = { "prettier" },
                 ["*"] = { "codespell" },
             },
             notify_on_error = true,
         },
     },
     --- Global search and replace
-    --- https://github.com/MagicDuck/grug-far.nvim 
+    --- https://github.com/MagicDuck/grug-far.nvim
     {
-        'MagicDuck/grug-far.nvim',
+        "MagicDuck/grug-far.nvim",
         keys = {
-            {'<leader>ss', '<cmd>:lua require("grug-far").open({ transient = true })<CR>', desc = 'Search & replace'},
-            {'<leader>S', '<cmd>:lua require("grug-far").toggle_instance({ instanceName="far", staticTitle="Find and Replace" })<CR>', desc = 'Toggle Grug-Far'},
-            {'<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = 'Search current word'},
-            {'<leader>sw', '<cmd>lua require("spectre").open_visual()<CR>', mode = "v", desc = 'Search current word'},
-            {'<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = 'Search on current file'},
+            { "<leader>ss", '<cmd>:lua require("grug-far").open({ transient = true })<CR>', desc = "Search & replace" },
+            {
+                "<leader>S",
+                '<cmd>:lua require("grug-far").toggle_instance({ instanceName="far", staticTitle="Find and Replace" })<CR>',
+                desc = "Toggle Grug-Far",
+            },
+            {
+                "<leader>sw",
+                '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+                desc = "Search current word",
+            },
+            { "<leader>sw", '<cmd>lua require("spectre").open_visual()<CR>', mode = "v", desc = "Search current word" },
+            {
+                "<leader>sp",
+                '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+                desc = "Search on current file",
+            },
         },
         opts = { headerMaxWidth = 100 },
     },
@@ -74,5 +118,5 @@ return {
     {
         "jake-stewart/multicursor.nvim",
         branch = "1.0",
-    }
+    },
 }

a73efc708d984751082e71e5293d790c9951cb3b
Axel H. <noirbizarre@gmail.com>
2024-11-19 02:15:54 +0100

    chore(lazy): update to latest versions

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "solarized-osaka" } },
+    dev = { path = "~/Workspace/neovim" },
     -- automatically check for plugin updates
     -- checker = { enabled = true },
 	performance = {

8ec2be903bdf1a95f1072f3291743a743897e387
Axel H. <noirbizarre@gmail.com>
2024-11-20 01:21:59 +0100

    chore(dashboard): minor dashboard adjustments

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -51,7 +51,6 @@ return {
                         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
                         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                        -- { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
                         { icon = " ", key = "s", desc = "Restore Session", section = "session" },
                         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
                         { icon = " ", key = "m", desc = "Mason", action = ":Mason" },
@@ -63,13 +62,14 @@ return {
                     {
                       pane = 2,
                       section = "terminal",
+                      -- cmd = "colorscript -e six",
                       cmd = "colorscript -e square",
                       height = 5,
                       padding = 1,
                     },
                     { section = "keys", gap = 1, padding = 1 },
                     { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                    -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
                     {
                       pane = 2,
                       icon = " ",

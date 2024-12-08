5f22e158b51d15bf8d52685b21bd4ddcb5846700
Axel H. <noirbizarre@gmail.com>
2024-12-08 03:15:55 +0100

    feat(1password): add a `lib.secrets.get` helper to fetch data from 1password

---
+++ lua/lib/secrets.lua
@@ -0,0 +1,13 @@
+local op = require("op.api")
+
+return {
+    -- Get a secret from 1password CLI (op)
+    get = function(url)
+        local stdout, stderr = op.read({url})
+        if #stderr > 0 then
+            Snacks.notify.error(stderr)
+        else
+            return stdout[0]
+        end
+    end,
+}

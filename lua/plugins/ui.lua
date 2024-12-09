d0377d79aad6787cb576585e157f2318de235e79
Axel H. <noirbizarre@gmail.com>
2024-12-09 14:44:29 +0100

    feat: configure Snacks Scratch buffers

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -36,6 +36,8 @@ return {
                 function() Snacks.bufdelete() end,
                 desc = "Delete Buffer",
             },
+            { "<leader>bs", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+            { "<leader>bS", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
             {
                 "<leader>gg",
                 function() Snacks.lazygit() end,

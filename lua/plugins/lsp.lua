de2fffeab73cae3d8f9822a4504739266abcbc8c
Axel H. <noirbizarre@gmail.com>
2024-12-15 15:48:36 +0100

    refactor(venv): use the new VenvSelector branch and scope to Python filetype

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -232,14 +232,16 @@ return {
     -- https://github.com/linux-cultist/venv-selector.nvim
     {
         "linux-cultist/venv-selector.nvim",
+        branch = "regexp",
         dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
         -- event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
         keys = {
             -- Keymap to open VenvSelector to pick a venv.
-            { "<leader>vs", "<cmd>VenvSelect<cr>" },
+            { "<leader>vs", "<cmd>VenvSelect<cr>", desc="Select virtualenv", ft = "python" },
             -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+            { "<leader>vc", "<cmd>VenvSelectCached<cr>", ft = "python" },
         },
+        ft = "python",
         opts = {
             name = { "venv", ".venv" },
         },

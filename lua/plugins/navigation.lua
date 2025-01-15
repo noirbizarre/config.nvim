210b430d06118c142f19c461f1820aa06156151d
Axel H. <noirbizarre@gmail.com>
2025-01-15 04:20:58 +0100

    feat(gx): easily open links and short links using `gx`

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -278,4 +278,83 @@ return {
         },
         opts = {},
     },
+    --- detect and open links
+    {
+        "chrishrb/gx.nvim",
+        keys = {
+            { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } },
+        },
+        cmd = { "Browse" },
+        opts = {
+            handlers = {
+                plugin = true,
+                github = true,
+                package_json = true,
+                search = true,
+                go = true,
+                jira = {
+                    name = "jira", -- set name of handler
+                    handle = function(mode, line, _)
+                        local ticket = require("gx.helper").find(line, mode, "(%u+-%d+)")
+                        if ticket and #ticket < 20 then
+                            return "https://ledgerhq.atlassian.net/browse/" .. ticket
+                        end
+                    end,
+                },
+                rust = {
+                    name = "rust",
+                    filetype = { "toml" },
+                    filename = "Cargo.toml",
+                    handle = function(mode, line, _)
+                        local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
+                        if crate then
+                            return "https://crates.io/crates/" .. crate
+                        end
+                    end,
+                },
+                pyproject = {
+                    name = "pyproject.toml",
+                    filetype = { "toml" },
+                    filename = "pyproject.toml",
+                    handle = function(mode, line, _)
+                        local helper = require("gx.helper")
+                        local pkg = helper.find(line, mode, '"([%w_%-]+)%s-[<>~=!]=')
+                            or helper.find(line, mode, '"([%w_%-]+)%[[%w_%-]+%]%s-[<>~=!]=')
+                        if pkg then
+                            return "https://pypi.org/project/" .. pkg
+                        end
+                    end,
+                },
+                pipfile = {
+                    name = "Pipfile",
+                    filetype = { "toml" },
+                    filename = "Pipfile",
+                    handle = function(mode, line, _)
+                        local helper = require("gx.helper")
+                        local pkg = (
+                            helper.find(line, mode, '^([%w_%-]+)%s-=%s-"[<>~=!]=')
+                            or helper.find(line, mode, '^([%w_%-]+)%s-=%s-"%*"')
+                            or helper.find(line, mode, '^([%w_%-]+)%s-=%s-%{.*version%s-=%s-"[<>~=!]=')
+                            or helper.find(line, mode, '^([%w_%-]+)%s-=%s-%{.*version%s-=%s-"%*"')
+                        )
+                        if pkg then
+                            return "https://pypi.org/project/" .. pkg
+                        end
+                    end,
+                },
+                requirements = {
+                    name = "requirements",
+                    filetype = { "requirements" },
+                    handle = function(mode, line, _)
+                        local helper = require("gx.helper")
+                        local pkg = helper.find(line, mode, "([%w_%-]+)%s-[<>~=!]=")
+                            or helper.find(line, mode, "([%w_%-]+)%[[%w_%-]+%]%s-[<>~=!]=")
+                        if pkg then
+                            return "https://pypi.org/project/" .. pkg
+                        end
+                    end,
+                },
+            },
+        },
+    },
 }

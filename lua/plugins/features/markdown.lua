1e506abb1d3e221b3ec3a02e3830f4af7f0e96c6
Axel H. <noirbizarre@gmail.com>
2025-01-25 18:14:59 +0100

    chore(markview): update to v25

--- lua/plugins/features/markdown.lua
+++ lua/plugins/features/markdown.lua
@@ -36,56 +36,61 @@ return {
             { "<leader>m>", function() require("markview.extras.headings").increase() end, desc = "Increase heading" },
         },
         opts = {
-            buf_ignore = {},
-            filetypes = { "markdown", "Avante", "codecompanion", "copilot-chat" },
-            modes = { "n", "i", "no", "c" },
-            hybrid_modes = { "i" },
-
-            callbacks = {
-                on_enable = function(_, win)
-                    vim.wo[win].conceallevel = 2
-                    vim.wo[win].concealcursor = "nc"
-                end,
+            preview = {
+                buf_ignore = {},
+                filetypes = { "markdown", "Avante", "codecompanion", "copilot-chat" },
+                modes = { "n", "i", "no", "c" },
+                hybrid_modes = { "i" },
+                icon_provider = "devicons",
+                callbacks = {
+                    on_enable = function(_, win)
+                        vim.wo[win].conceallevel = 2
+                        vim.wo[win].concealcursor = "nc"
+                    end,
+                },
             },
-            links = {
-                inline_links = {
-                    hl = "@markup.link.label.markown_inline",
-                    icon = " ",
-                    icon_hl = "@markup.link",
+            markdown = {
+                code_blocks = {
+                    default = {
+                        block_hl = "ColorColumn",
+                        pad_hl = "ColorColumn",
+                    },
+                    border_hl = "ColorColumn",
+                    info_hl = "ColorColumn",
+                    pad_amount = 0,
+                },
+                headings = {
+                    heading_1 = { hl = "DiffAdd", icon_hl = "DiffAdd" },
+                    heading_2 = { hl = "DiffChange", icon_hl = "DiffChange" },
+                    heading_3 = { hl = "DiffDelete", icon_hl = "@markup.heading.3.markdown" },
+                    heading_4 = { hl = "DiffDelete", icon_hl = "@markup.heading.4.markdown" },
+                    heading_5 = { hl = "DiffDelete", icon_hl = "@markup.heading.5.markdown" },
+                    heading_6 = { hl = "DiffDelete", icon_hl = "@markup.heading.6.markdown" },
+                },
+                list_items = {
+                    shift_width = 2,
+                    marker_minus = { text = "●", hl = "@markup.list.markdown" },
+                    marker_plus = { text = "●", hl = "@markup.list.markdown" },
+                    marker_star = { text = "●", hl = "@markup.list.markdown" },
+                    marker_dot = {},
+                },
+            },
+            markdown_inline = {
+                hyperlinks = {
+                    default = {
+                        hl = "@markup.link.label.markown_inline",
+                    },
                 },
                 images = {
-                    hl = "@markup.link.label.markown_inline",
-                    icon = " ",
-                    icon_hl = "@markup.link",
+                    default = {
+                        hl = "@markup.link.label.markown_inline",
+                    },
                 },
-            },
-            code_blocks = {
-                style = "language",
-                icons = "devicons",
-                hl = "ColorColumn",
-                info_hl = "ColorColumn",
-                pad_amount = 0,
-            },
-            inline_codes = {
-                hl = "RenderMarkdownCode",
-                padding_left = 0,
-                padding_right = 0,
-            },
-            list_items = {
-                shift_width = 2,
-                marker_minus = { text = "●", hl = "@markup.list.markdown" },
-                marker_plus = { text = "●", hl = "@markup.list.markdown" },
-                marker_star = { text = "●", hl = "@markup.list.markdown" },
-                marker_dot = {},
-            },
-            -- inline_codes = { enable = false },
-            headings = {
-                heading_1 = { hl = "DiffAdd", icon_hl = "DiffAdd" },
-                heading_2 = { hl = "DiffChange", icon_hl = "DiffChange" },
-                heading_3 = { hl = "DiffDelete", icon_hl = "@markup.heading.3.markdown" },
-                heading_4 = { hl = "DiffDelete", icon_hl = "@markup.heading.4.markdown" },
-                heading_5 = { hl = "DiffDelete", icon_hl = "@markup.heading.5.markdown" },
-                heading_6 = { hl = "DiffDelete", icon_hl = "@markup.heading.6.markdown" },
+                inline_codes = {
+                    hl = "RenderMarkdownCode",
+                    padding_left = 0,
+                    padding_right = 0,
+                },
             },
         },
         config = function(_, opts)

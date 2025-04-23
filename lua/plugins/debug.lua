8944519e74567c3bc0b176e49532f9fd17f74bfa
Axel H. <noirbizarre@gmail.com>
2025-04-23 02:28:59 +0200

    feat(debug): use my new contributed `nvim-dap-view` features

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -11,11 +11,56 @@ return {
             },
             {
                 "igorlfs/nvim-dap-view",
-                branch = "feat/scopes",
+                dev = true,
                 opts = {
                     winbar = {
                         sections = { "scopes", "watches", "exceptions", "breakpoints", "threads", "repl" },
                         default_section = "scopes",
+                        headers = {
+                            breakpoints = " Breakpoints",
+                            scopes = " Scopes",
+                            exceptions = " Exceptions",
+                            watches = "  Watches",
+                            threads = " Threads",
+                            repl = " REPL",
+                            console = " Console",
+                        },
+                        controls = {
+                            enabled = true,
+                            position = "left",
+                            buttons = {
+                                "play",
+                                "step_into",
+                                "step_over",
+                                "step_out",
+                                "term_restart",
+                            },
+                            custom_buttons = {
+                                term_restart = {
+                                    render = function()
+                                        local session = require("dap").session()
+                                        local group = session and "ControlTerminate" or "ControlRunLast"
+                                        local icon = session and "" or ""
+                                        return "%#NvimDapView" .. group .. "#" .. icon .. "%*"
+                                    end,
+                                    action = function(clicks, button, modifiers)
+                                        local dap = require("dap")
+                                        local alt = clicks > 1 or button ~= "l" or modifiers:gsub(" ", "") ~= ""
+                                        if not dap.session() then
+                                            dap.run_last()
+                                        elseif alt then
+                                            dap.disconnect()
+                                        else
+                                            dap.terminate()
+                                        end
+                                    end,
+                                },
+                            },
+                            icons = {
+                                play = "",
+                                terminate = "",
+                            },
+                        },
                     },
                 },
             },

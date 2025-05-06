1686c79e3e8876c6cb6209da97ba385d622fa4a0
Axel H. <noirbizarre@gmail.com>
2025-05-06 15:59:37 +0200

    feat(jinja): tree-sitter only highlighting

--- lua/setup/filetypes.lua
+++ lua/setup/filetypes.lua
@@ -20,8 +20,26 @@ vim.filetype.add({
         [".*/%.config/tmux/.+%.conf"] = "tmux",
         -- ["%.env%.[%w_.-]+"] = "sh",
         [".*/.+%.secret"] = "sh",
+        -- Known templates
+        [".*/copier%.yml"] = "jinja.yaml",
     },
 })
 
 vim.treesitter.language.register("bash", "kitty")
 vim.treesitter.language.register("properties", { "jproperties" })
+
+vim.treesitter.query.add_directive("inject-jinja!", function(_, _, bufnr, _, metadata)
+    if type(bufnr) ~= "number" or not vim.api.nvim_buf_is_valid(bufnr) then
+        return
+    end
+    local basename = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+    for _, ext in ipairs({ "jinja", "jinja2", "j2" }) do
+        basename = basename:gsub("%." .. ext, "")
+    end
+
+    local ft = vim.filetype.match({ filename = basename })
+    if ft ~= nil then
+        local raw_ft = ft:gsub("jinja", ""):gsub("%.", "")
+        metadata["injection.language"] = raw_ft
+    end
+end, {})
---
+++ queries/jinja/injections.scm
@@ -0,0 +1,9 @@
+;; extends
+
+((words) @injection.content
+  (#inject-jinja!)
+  (#set! injection.combined))
+
+; ((inline) @injection.content
+;   (#inject-jinja!)
+;   (#set! injection.combined))

cf8a9a1f7ec6e0c2608cb71c712e3fde7f229c88
Axel H. <noirbizarre@gmail.com>
2025-08-21 23:01:04 +0200

    feat(http): initial kulala http config

---
+++ lua/plugins/features/http.lua
@@ -0,0 +1,16 @@
+return {
+    "mistweaverco/kulala.nvim",
+    keys = {
+        { "<leader>Rs", desc = "Send request" },
+        { "<leader>Ra", desc = "Send all requests" },
+        { "<leader>Rb", desc = "Open scratchpad" },
+        { "<leader>Ro", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+        -- your configuration comes here
+        global_keymaps = true,
+        global_keymaps_prefix = "<leader>R",
+        kulala_keymaps_prefix = "",
+    },
+}

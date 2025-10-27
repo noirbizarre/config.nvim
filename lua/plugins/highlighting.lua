ac3bfd0efa2b7d4041c35823d9489057b42bd463
Axel H. <noirbizarre@gmail.com>
2025-10-27 02:53:53 +0100

    feat(treesitter): incremental selection and textobjects

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -104,6 +104,78 @@ return {
             require("nvim-treesitter").update()
         end,
     },
+    -- Incremental selection
+    {
+        "MeanderingProgrammer/treesitter-modules.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Enter>",
+                    node_incremental = "<Enter>",
+                    scope_incremental = false,
+                    node_decremental = "<BS>",
+                },
+            },
+        },
+    },
+    -- Custom Textobjects
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        keys = function()
+            local keys = {
+                --- Swaps
+                {
+                    "<leader>rsp",
+                    function() require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner") end,
+                    desc = "Swap with next parameter",
+                },
+                {
+                    "<leader>rsP",
+                    function() require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner") end,
+                    desc = "Swap with previous parameter",
+                },
+            }
+
+            local selects = {
+                { "af", "@function.outer" },
+                { "if", "@function.inner" },
+                { "ac", "@class.outer" },
+                { "ic", "@class.inner" },
+                { "ap", "@parameter.outer" },
+                { "ip", "@parameter.inner" },
+                { "al", "@loop.outer" },
+                { "il", "@loop.inner" },
+                { "ab", "@block.outer" },
+                { "ib", "@block.inner" },
+                { "as", "@local.scope" },
+            }
+            for _, obj in ipairs(selects) do
+                keys[#keys + 1] = {
+                    obj[1],
+                    function() require("nvim-treesitter-textobjects.select").select_textobject(obj[2], "textobjects") end,
+                    mode = { "x", "o" },
+                    desc = obj[2],
+                }
+            end
+
+            return keys
+        end,
+        opts = {
+            select = {
+                enable = true,
+                lookahead = true,
+                selection_modes = {
+                    ["@parameter.outer"] = "v", -- charwise
+                    ["@function.outer"] = "V", -- linewise
+                    ["@class.outer"] = "<c-v>", -- blockwise
+                },
+            },
+        },
+    },
 
     -- Adjust indent with treesitter injections
     {

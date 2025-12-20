b6d660d28c0c353c64e320f15f1d154dfc98290b
Axel H. <noirbizarre@gmail.com>
2025-12-20 12:22:10 +0100

    feat(snacks): added missing command picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -124,7 +124,8 @@ return {
             { "<leader>kh", function() Snacks.picker.help() end, desc = "Help Pages" },
             { "<leader>kH", function() Snacks.picker.highlights() end, desc = "Highlights" },
             { "<leader>kq", function() Snacks.picker.qflist() end, desc = "Quickfixes" },
-            { "<leader>kc", function() Snacks.picker.command_history() end, desc = "Command history" },
+            { "<leader>kc", function() Snacks.picker.commands() end, desc = "Commands" },
+            { "<leader>kC", function() Snacks.picker.command_history() end, desc = "Command history" },
             { "<leader>ku", function() Snacks.picker.undo() end, desc = "Undo tree" },
             { "<leader>kl", function() Snacks.picker.spelling() end, desc = "Spelling" },
             { "<leader>kv", function() Snacks.picker.registers() end, desc = "Registers" },
@@ -354,6 +355,11 @@ return {
                                 "Struct",
                                 "Trait",
                             },
+                        },
+                    },
+                    explorer = {
+                        layout = {
+                            hidden = { "input", "preview" },
                         },
                     },
                 },

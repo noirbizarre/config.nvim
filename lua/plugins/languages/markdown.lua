9225b44f4f61d12d9a1a747d302d467266808c67
Axel H. <noirbizarre@gmail.com>
2026-01-14 02:47:38 +0100

    fix(markdown): Update config to latest changes

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -11,6 +11,48 @@ return {
             },
         },
     },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        opts = {
+            highlights = {
+                ["@markup.heading"] = function(highlights, colors)
+                    local util = require("solarized-osaka.util")
+                    highlights["@markup.heading.1"] = {
+                        fg = colors.blue,
+                        bg = util.blend(colors.blue, colors.bg, 0.25),
+                        bold = true,
+                    }
+                    highlights["@markup.heading.2"] = {
+                        fg = colors.orange,
+                        bg = util.blend(colors.orange100, colors.bg, 0.25),
+                        bold = true,
+                    }
+                    highlights["@markup.heading.3"] = {
+                        fg = colors.yellow,
+                        bg = util.blend(colors.yellow, colors.bg, 0.25),
+                        bold = true,
+                    }
+                    highlights["@markup.heading.4"] = {
+                        fg = colors.green,
+                        bg = util.blend(colors.green, colors.bg, 0.25),
+                        bold = true,
+                    }
+                    highlights["@markup.heading.5"] = {
+                        fg = colors.magenta,
+                        bg = util.blend(colors.magenta100, colors.bg, 0.25),
+                        bold = true,
+                    }
+                    highlights["@markup.heading.6"] = {
+                        fg = colors.violet,
+                        bg = util.blend(colors.violet, colors.bg, 0.25),
+                        bold = true,
+                    }
+
+                    return { fg = colors.cyan, bg = util.blend(colors.cyan, colors.bg, 0.25) }
+                end,
+            },
+        },
+    },
     -- Inline Markdown rendering
     {
         "OXY2DEV/markview.nvim",
@@ -21,16 +63,14 @@ return {
         },
         cmd = {
             "Markview",
-            "MarkOpen",
-            "CodeCreate",
-            "CodeEdit",
-            "CheckboxToggle",
+            "Editor",
+            "Checkbox",
         },
         ft = filetypes,
         keys = {
-            { "<leader>mc", "<cmd>CodeCreate<cr>", desc = "Create code block" },
-            { "<leader>me", "<cmd>CodeEdit<cr>", desc = "Edit code block" },
-            { "<leader>mt", "<cmd>CheckboxToggle<cr>", desc = "Toggle checkbox" },
+            { "<leader>mc", function() require("markview.extras.editor").create() end, desc = "Create code block" },
+            { "<leader>me", function() require("markview.extras.editor").edit() end, desc = "Edit code block" },
+            { "<leader>mt", "<cmd>Checkbox toggle<cr>", desc = "Toggle checkbox" },
             { "<leader>m<", function() require("markview.extras.headings").decrease() end, desc = "Decrease heading" },
             { "<leader>m>", function() require("markview.extras.headings").increase() end, desc = "Increase heading" },
         },
@@ -54,12 +94,12 @@ return {
                     pad_amount = 0,
                 },
                 headings = {
-                    heading_1 = { hl = "DiffAdd", icon_hl = "DiffAdd" },
-                    heading_2 = { hl = "DiffChange", icon_hl = "DiffChange" },
-                    heading_3 = { hl = "DiffDelete", icon_hl = "@markup.heading.3.markdown" },
-                    heading_4 = { hl = "DiffDelete", icon_hl = "@markup.heading.4.markdown" },
-                    heading_5 = { hl = "DiffDelete", icon_hl = "@markup.heading.5.markdown" },
-                    heading_6 = { hl = "DiffDelete", icon_hl = "@markup.heading.6.markdown" },
+                    heading_1 = { hl = "@markup.heading.1.markdown", icon_hl = "@markup.heading.1.markdown" },
+                    heading_2 = { hl = "@markup.heading.2.markdown", icon_hl = "@markup.heading.2.markdown" },
+                    heading_3 = { hl = "@markup.heading.3.markdown", icon_hl = "@markup.heading.3.markdown" },
+                    heading_4 = { hl = "@markup.heading.4.markdown", icon_hl = "@markup.heading.4.markdown" },
+                    heading_5 = { hl = "@markup.heading.5.markdown", icon_hl = "@markup.heading.5.markdown" },
+                    heading_6 = { hl = "@markup.heading.6.markdown", icon_hl = "@markup.heading.6.markdown" },
                 },
                 list_items = {
                     shift_width = 2,

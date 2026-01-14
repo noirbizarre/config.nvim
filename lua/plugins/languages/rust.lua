2d662194e93be4a06569b4bebf64c4194d9f0053
Axel H. <noirbizarre@gmail.com>
2026-01-14 01:14:44 +0100

    fix(rust): disable ensure auto-install on rust due to Rustacean

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -31,6 +31,13 @@ return {
             ignore = {
                 packages = { "rustfmt" },
             },
+            lsp = {
+                auto = {
+                    ignore = {
+                        rust = "*",
+                    },
+                },
+            },
         },
     },
 }
